141221b6d7105c54486c176f1725cadbfb8c0f25
darryl-dg <darrylgrierjr@gmail.com>
2022-08-24 17:19:28 -0700

    updated linter, added gitignore and added Reviews.jsx file

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>);
